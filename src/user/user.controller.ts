6c9dd86d99222746d00511f8edf0a57fc58b053c
Sammy <samuelazevedo931@gmail.com>
2026-01-02 15:00:09 -0300

    feat: user and database configuration
    + add moduleFormat = "cjs"
    To fix es module error

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Prisma, User as UserModel } from 'generated/prisma/client';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    @Inject()
+    private readonly userService: UserService;
+
+    @Post('signup')
+    async signupUser(
+        @Body() userData: Prisma.UserCreateInput,
+    ): Promise<UserModel> {
+        return this.userService.createUser(userData);
+    }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  controllers: [UserController]
+  imports: [DatabaseModule],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class UserModule {}
